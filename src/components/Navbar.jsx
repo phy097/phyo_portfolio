e83c992df64067a4bb2676d559cf03232a29ff7b
Phyo <phyoeikyaw210@gmail.com>
2024-08-10 10:42:05 +0700

    changing file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import phyo from "../assets/me.jpeg";
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaLine } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
+import { FaViber } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -11,10 +11,10 @@ const Navbar = () => {
             <img className='mx-2 w-14 h-14 rounded-full' src={phyo} alt="phyo"/>
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-            <a href="#"><FaLinkedin/></a>
-            <FaGithub/>
-            <FaLine/>
-            <FaInstagram/>
+            <a href=" https://www.linkedin.com/in/phyoeikyaw/"><FaLinkedin/></a>
+            <a href=" https://github.com/phy097"><FaGithub/></a>
+            <a href="https://line.me/ti/p/kuATapjAxc"><FaLine/></a>
+            <a href="https://vb.me/letsChatOnViber"><FaViber/></a>
         </div>
     </nav>
   );
